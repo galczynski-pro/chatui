e295945f150dca1a8fca1f6ef8521318d1333b07
Gpro <matthew@galczynski.pro>
2025-09-04 16:09:49 +0100

    Update speech.js

--- js/speech.js
+++ js/speech.js
@@ -1,63 +1,36 @@
-﻿var bTextToSpeechSupported = false;
-var bSpeechInProgress = false;
-var oSpeechRecognizer = null
+var bTextToSpeechSupported = false;
 var oSpeechSynthesisUtterance = null;
 var oVoices = null;
+window.voiceEnabled = false; // controlled from navbar toggle
 
 function OnLoad() {
-//Load Data from Chat Charater JSON to Select
-         //Load Voice 
-             if ("webkitSpeechRecognition" in window) {
-    } else {
-        //speech to text not supported
-        lblSpeak.style.display = "none";
-    }
     if ('speechSynthesis' in window) {
         bTextToSpeechSupported = true;
         speechSynthesis.onvoiceschanged = function () {
             oVoices = window.speechSynthesis.getVoices();
-            for (var i = 0; i < oVoices.length; i++) {
-                selVoices[selVoices.length] = new Option(oVoices[i].name, i);
-            }
         };
     }
 }
-function TextToSpeech(s) {
-    if (bTextToSpeechSupported == false) return;
+function TextToSpeech(s) { // manual play, always speak if supported
+    if (!bTextToSpeechSupported) return;
     oSpeechSynthesisUtterance = new SpeechSynthesisUtterance();
-    if (oVoices) {
-        var sVoice = selVoices.value;
-        if (sVoice != "") {
-            oSpeechSynthesisUtterance.voice = oVoices[parseInt(sVoice)];
-        }
+    if (oVoices && oVoices.length) {
+        oSpeechSynthesisUtterance.voice = oVoices[0];
     }
-    oSpeechSynthesisUtterance.onend = function () {
-        //finished talking - can now listen
-        if (oSpeechRecognizer && chkSpeak.checked) {
-            oSpeechRecognizer.start();
-        }
-    }
-    if (oSpeechRecognizer && chkSpeak.checked) {
-        //do not listen to yourself when talking
-        oSpeechRecognizer.stop();
-    }
-    oSpeechSynthesisUtterance.lang = "en-US";
+    oSpeechSynthesisUtterance.lang = "pl-PL"; // default to Polish
     oSpeechSynthesisUtterance.text = s;
-    //Uncaught (in promise) Error: A listener indicated an 
-    //asynchronous response by returning true, but the message channel closed 
     window.speechSynthesis.speak(oSpeechSynthesisUtterance);
 }
-//function ply(button){
-//	 $div = button.parent("p");
-//    text = $div.find("span").text();
-//    console.log('xx');
-//          }
+function SpeakIfEnabled(s) { // auto play only when enabled
+    if (window.voiceEnabled) {
+        TextToSpeech(s);
+    }
+}
+// Click-to-play remains available
 $(document).ready(function(){
-$(document).on("click", ".play", function(e){
- $div = $(this).parent("p");
- id = $(this).attr("vall");
-    text = $div.find("span").text();
-    console.log(id);
+  $(document).on("click", ".play", function(e){
+    var id = $(this).attr("vall");
     TextToSpeech(id);
+  });
 });
-});
+
